0cccd9c1f113aba7b8f3eff930b69ddf46c8236d
hyrumjackson <hyrumjjackson@gmail.com>
2024-04-08 12:59:43 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+using System.Diagnostics;
+using BuildsByBrickwell.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildsByBrickwell.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
