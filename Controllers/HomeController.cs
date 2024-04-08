66466bc0c0dbcf16a2c0c61e12d33b073e3f43cd
JadenGatherum <jadenbb@byu.edu>
2024-04-08 17:50:23 -0600

    i dont want to commit this

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -43,11 +43,6 @@ namespace BuildsByBrickwell.Controllers
             return View();
         }
 
-        public IActionResult Testing()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
